6795918f168d30bb384b4dcdceacbfbbd9f9992a
yaoone <729299641@qq.com>
2019-06-26 15:33:41 +0800

    day3

--- day3/torjan/hack.js
+++ day3/torjan/hack.js
@@ -1,4 +1,26 @@
-var imgs = document.querySelectorAll("img");
-for(var i = 0; i< imgs.length;i++){
-	imgs[i].src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1126537924,4258824202&fm=27&gp=0.jpg"
-}
+// 获取所有网页图片，把图片吧修改木马的图片
+// var imgs = document.querySelectorAll("img");
+// for(var i = 0; i< imgs.length;i++){
+// 	imgs[i].src="https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1126537924,4258824202&fm=27&gp=0.jpg"
+// }
+
+// // 更改百度按钮
+// document.querySelector('#su').style.backgroundColor = 'red'
+
+// // 监听输入框的内容
+// document.querySelector('#kw').addEventListener('keyup',function(e){
+// 	console.log(e.target.value)
+// })
+
+// 自动输入QQ的密码
+// document.querySelector('#u').value="123456"
+// document.querySelector('#p').value="123456"
+
+// document.querySelector("#go").click()
+// 把logo给删除
+// $("#logo").remove()
+setInterval(function(){
+	$("#k_s_ol_chatWin").remove()
+},500)
+
+// console.log($("#logo"))
